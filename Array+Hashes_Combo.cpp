272de3f074d96250dbf4adb54730c3d45475bee0
nayan32006 <nayang389@gmail.com>
2026-04-10 15:22:25 +0530

    Array+Hashes_Combo done

---
+++ Array+Hashes_Combo.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n= nums.size();
+        int count=0 ;
+
+        for(int i=0; i<n; i++)
+        {
+            int sum = 0;
+            for(int j=i; j<n; j++)
+            {
+                sum +=nums[j];
+                if(sum == k) count++;
+            }
+
+        
+        }
+        return count;                     //tc =0(n)
+                                          //sc=0(n)
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> nums={1,1,1};
+    int k= 2;
+    cout << "Result: " << sol.subarraySum(nums, k) << endl;
+    return 0;
+
+}
